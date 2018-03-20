d914cbc348f7b8a071bde6455e2ccbdfd04c1f73
Madhav Bahl <madhavbahl10@gmail.com>
2018-03-20 10:18:06 +0530

    Set up the server

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
 # App-A-Thon Portal
 
 This is the official repository for App-A-Thon portal
+
+## Database Schema
+
+* Name 1,2,3 --
+
+* Roll Number -- 
+
+* Leader email id
+
+* Team name -- 
+
+* Product Name --
+
+* Description --
+
+* GitHub Link/ Drive Link
+
---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const hbs = require('hbs');
+
+const port = process.env.PORT || 8000;
+
+var app = express();
+app.use(bodyParser());
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'hbs');
+
+/* =========================================== */
+/* ===== Temprary route for dev purposes ===== */
+/* =========================================== */
+
+/* ===== End of user based temprary route ===== */
+
+app.get('/', (req, res) => {
+    res.render('index.hbs', {title: 'Hello World!'});
+}); 
+
+app.listen (port, () => {
+    console.log(`Server is up on port ${port}`);
+})
