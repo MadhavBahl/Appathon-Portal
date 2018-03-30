b76d53933dff956ae4b3957a863426201f417743
Madhav Bahl <madhavbahl10@gmail.com>
2018-03-31 00:04:36 +0530

    Add DELETE /Rounds

--- serverFiles/deleteAllRound.js
+++ serverFiles/deleteAllRound.js
@@ -1,7 +1,7 @@
 const {mongoose} = require('./mongoose');
 const {Round} = require('./part2schema');
 
-const deleteAllRev = (callback) => {
+const deleteAllRound = (callback) => {
     Round.remove({}, (err) => {
         if (err) {
             return console.log(err);
